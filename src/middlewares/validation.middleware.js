5cb3f5b3dbefdc22cbc0ed1e274ca01d2adabf9e
ds asante <dsasante1@gmail.com>
2023-09-20 21:29:24 +0000

    feat: user signup

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -6,20 +6,31 @@ const  { responseProvider }  = require('../../helper/response');
 const checkCreateUserInput = (req, res, next) => {
 
   try {
-    const { email, username, password } = req.body;
+    const { email, firstName, lastName, password, phoneNumber } = req.body;
 
 
     if (typeof email !== 'string' || !email.includes('@')) {
       return responseProvider( res, null, 'provide a valid email', 400)
     }
 
-    if (typeof username !== 'string') {
-      return responseProvider( res, null, 'provide a valid username', 400)
+    if (typeof firstName !== 'string' || !firstName) {
+      return responseProvider( res, null, 'provide a valid firstName', 400)
     }
+
+
+    if (typeof lastName !== 'string' || !lastName) {
+      return responseProvider( res, null, 'provide a valid lastName', 400)
+    }
+
 
     if (typeof password !== 'string' || password.length < 8) {
       return responseProvider( res, null, 'provide a valid password', 400)
     }
+
+    if (typeof phoneNumber !== 'number' || password.length < 10) {
+      return responseProvider( res, null, 'provide a valid phone number', 400)
+    }
+
 
     return next();
   } catch (error) {
