3731b784879c5a7d77fc12c487f32f5af179d975
sylvia dugah <sylvialyril.sl@gmail.com>
2023-09-22 19:48:21 +0000

    updated

--- src/routes/applicant.routes.js
+++ src/routes/applicant.routes.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 
-const { checkSignUpApplicantInput, checkApplicantLoginInput} = require('../middlewares/validation.middleware')
-const { applicantImageUploader, applicantDocUploader } = require('../middlewares/applicant.middleware')
+const { 
+  checkSignUpApplicantInput,
+   checkApplicantLoginInput, 
+   checkApplicationInput
+  } = require('../middlewares/validation.middleware')
+
+const { 
+  applicantImageUploader, 
+  applicantDocUploader,
+  setApplicantImageDb,
+  setApplicantDocDb
+
+} = require('../middlewares/applicant.middleware')
 
 
-const {  uploadUserImgUtil } = require('../../utils/applicant.img.upload');
-const multer = require('multer');
-const  uploadApplicantImg = multer({  uploadUserImgUtil });
-
+const { imgUpload, pdfUpload } = require("../../utils/multer");
 
 
 const {
@@ -19,14 +27,76 @@ const {
 
 
 
-//router.post('/upload', uploadUserImg.single('image'), applicantImageUploader );
-
-
 router.post('/signup', checkSignUpApplicantInput, createApplicant);
 
-router.post('/application',  uploadApplicantImg.single('image'), applicantImageUploader, applicantDocUploader)
+
+
+router.post('/upload', checkApplicationInput, imgUpload.single("image"),
+ applicantImageUploader, setApplicantImageDb, pdfUpload.single("pdf"), 
+ applicantDocUploader, setApplicantDocDb)
 
 router.post('/login', checkApplicantLoginInput, signInApplicant);
 
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const router = express.Router();
+
+
+// const { checkSignUpApplicantInput, checkApplicantLoginInput} = require('../middlewares/validation.middleware')
+// const { applicantImageUploader, applicantDocUploader } = require('../middlewares/applicant.middleware')
+
+
+// const {  uploadUserImgUtil } = require('../../utils/applicant.img.upload');
+// const multer = require('multer');
+// const  uploadApplicantImg = multer({  uploadUserImgUtil });
+
+
+
+// const {
+//   createApplicant,
+//   signInApplicant,
+// } = require('../controllers/applicant.controllers');
+
+
+
+// //router.post('/upload', uploadUserImg.single('image'), applicantImageUploader );
+
+
+// router.post('/signup', checkSignUpApplicantInput, createApplicant);
+
+// router.post('/application',  uploadApplicantImg.single('image'), applicantImageUploader, applicantDocUploader)
+
+// router.post('/login', checkApplicantLoginInput, signInApplicant);
+
+
+// module.exports = router;
