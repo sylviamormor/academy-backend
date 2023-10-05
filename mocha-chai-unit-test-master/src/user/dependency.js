dd0f93be95bb31401c09cbc5f9d2fa337b0de0bc
ds asante <dsasante1@gmail.com>
2023-10-05 14:59:58 +0000

    chore: remove extra folders

--- mocha-chai-unit-test-master/src/user/dependency.js
+++
@@ -1,9 +0,0 @@
-const UserRepository = require("./user.repository");
-const UserService = require("./user.service");
-
-const userRepository = new UserRepository();
-const userService = new UserService(userRepository);
-module.exports = {
-  userRepository,
-  userService
-};
